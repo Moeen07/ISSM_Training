9f1808b4314de117fd11f3391df32425166a4f06
Moeen07 <moeenmoonis57@gmail.com>
2024-01-23 13:14:44 +0500

    Tested fs module

---
+++ Week 1/Day 2/fs-module.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+
+fs.readFile("Test.txt", "utf8", (err, data) => {
+  console.log(err, data);
+});
+
+// const a = fs.readFileSync("Test.txt")
+// console.log(a.toString)
+
+console.log("Finished reading file"); // To demonstrate asynchronous nature of Node
+
+fs.writeFile("Test.txt", "New data inserted", () => {
+  console.log("Written to the file");
+});
