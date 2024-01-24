757f74b62ccabdeddbbae4de13840369acec5503
Moeen07 <moeenmoonis57@gmail.com>
2024-01-24 12:40:03 +0500

    Operators

---
+++ Week 1/Day 3/operators.js
@@ -0,0 +1,29 @@
+// Modulus(Remainder)
+console.log(17 % 5);
+
+// Exponentiation(Power)
+console.log(4 ** 3);
+
+// Assignment Operators
+
+let a = 5;
+let b = 7;
+
+a += 4; // a = a + 4
+console.log(a);
+
+b += a; // b = b + a
+console.log(b);
+
+// Comparison Operators
+
+// == ... Equal to
+// === .... Equal to and type
+// "5" will be converted to integer by javascript so 5 == "5" will be true
+// Use === to also check the data type
+
+// Ternary Operator
+// condition ? true : false
+let age = 24;
+
+console.log(age > 20 ? "adult" : "not adult");
