ff3a04c16d09ba47e740f8ade442932f2c6e8389
Moeen07 <moeenmoonis57@gmail.com>
2024-01-25 16:28:32 +0500

    Event Listeners

--- Week 1/Day 4/event-handler.js
+++ Week 1/Day 4/event-handler.js
@@ -1,10 +1,10 @@
-let bt1 = document.querySelector("#btn1");
+let btn1 = document.querySelector("#btn1");
 let btn2 = document.querySelector("#btn2");
 let box = document.querySelector("#box");
 
-btn1.onclick = () => {
-  console.log("Button was clicked");
-};
+// btn1.onclick = () => {
+//   console.log("Button was clicked");
+// };
 
 btn2.onclick = () => {
   console.log("Button was clicked twice!");
@@ -16,3 +16,21 @@ box.onmouseover = (e) => {
   console.log(e.target);
   console.log(e.type);
 };
+// Event listeners are useful because we can create multiple listeners thus enabling ourselves to do multiple tasks
+// on a single event
+btn1.addEventListener("click", (e) => {
+  console.log("Button 1 was clicked");
+  console.log(e.type);
+});
+btn1.addEventListener("click", (e) => {
+  console.log("Button 1 was clicked - handler 2");
+});
+
+const handler3 = () => {
+  console.log("Button 1 was clicked - handler 3");
+};
+
+btn1.addEventListener("click", handler3);
+
+// Removing event listener
+btn1.removeEventListener("click", handler3);
