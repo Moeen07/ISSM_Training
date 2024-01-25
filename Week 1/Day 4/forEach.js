cb45fd67a2d942f141e5e52ac6a3af9484541d09
Moeen07 <moeenmoonis57@gmail.com>
2024-01-25 13:11:58 +0500

    for-Each

---
+++ Week 1/Day 4/forEach.js
@@ -0,0 +1,8 @@
+// forEach
+// forEach.arr(callBack)...this callBack function will execute for each element in an array
+
+let arr = [1, 2, 3, 4, 5, 6];
+
+arr.forEach((val) => {
+  console.log(val * 4);
+});
