a96af60662da3128148ee3bf5f4ed66142896e08
Moeen07 <moeenmoonis57@gmail.com>
2024-01-25 13:43:56 +0500

    Practice Question 5

---
+++ Week 1/Day 4/practice-question5.js
@@ -0,0 +1,17 @@
+let num = 5;
+let arr = new Array(num);
+for (let i = 0; i < num; i++) {
+  arr[i] = i + 1;
+}
+console.log(arr);
+
+const sum = arr.reduce((prev, curr) => {
+  return prev + curr;
+});
+
+const product = arr.reduce((prev, curr) => {
+  return prev * curr;
+});
+
+console.log(`Sum is ${sum}`);
+console.log(`Product is ${product}`);
