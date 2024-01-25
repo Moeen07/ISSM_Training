4ac5ab4c8fee89334d5f49e24814c75517287c7a
Moeen07 <moeenmoonis57@gmail.com>
2024-01-25 16:15:59 +0500

    Tested event object

--- Week 1/Day 4/event-handler.js
+++ Week 1/Day 4/event-handler.js
@@ -1,5 +1,18 @@
 let bt1 = document.querySelector("#btn1");
+let btn2 = document.querySelector("#btn2");
+let box = document.querySelector("#box");
 
 btn1.onclick = () => {
-  console.log("button was clicked");
+  console.log("Button was clicked");
+};
+
+btn2.onclick = () => {
+  console.log("Button was clicked twice!");
+};
+
+// e is event object
+box.onmouseover = (e) => {
+  console.log("Mouse was hovered here");
+  console.log(e.target);
+  console.log(e.type);
 };
