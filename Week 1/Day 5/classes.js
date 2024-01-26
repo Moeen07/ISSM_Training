9ecb7b6274406efd7cc054acbdab2ab33be73916
Moeen07 <moeenmoonis57@gmail.com>
2024-01-26 13:11:02 +0500

    Constructor Method

--- Week 1/Day 5/classes.js
+++ Week 1/Day 5/classes.js
@@ -12,8 +12,23 @@ class ToyotaCar {
   }
 }
 
-let prius = new ToyotaCar();
+let prius = new ToyotaCar(); // new invokes the constructor
 let corolla = new ToyotaCar();
 
 prius.setType("Hybrid");
 console.log(prius.typeName);
+
+class HondaCar {
+  constructor(type) {
+    console.log("Creating new Honda vehicle");
+    this.type = type;
+  }
+  start() {
+    console.log("start");
+  }
+  stop() {
+    console.log("stop");
+  }
+}
+// if we want to do something with our object at the time of its creation/intialization then we can use constructor
+let civic = new HondaCar("Civic");
