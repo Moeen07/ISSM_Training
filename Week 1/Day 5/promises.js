e2f7cc32ebc700c3d4802ac22cfa71fa80e99a64
Moeen07 <moeenmoonis57@gmail.com>
2024-01-26 15:05:54 +0500

    Resolve Promise

--- Week 1/Day 5/promises.js
+++ Week 1/Day 5/promises.js
@@ -1,4 +1,28 @@
-let promise = new Promise((resolve, reject) => {
-  console.log("This is a promise");
-  resolve("Successfully Resolved");
-});
+// let promise = new Promise((resolve, reject) => {
+//   console.log("This is a promise");
+//   resolve("Successfully Resolved");
+// });
+
+// Suppose this is an API
+function getData(dataId, getNextData) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("data", dataId);
+      resolve("success..!");
+      if (getNextData) {
+        getNextData();
+      }
+    }, 3000);
+  });
+}
+
+result = getData(25);
+console.log(result);
+
+//   getData(1, () => {
+//     getData(2, () => {
+//       getData(3, () => {
+//         getData(4);
+//       });
+//     });
+//   });
