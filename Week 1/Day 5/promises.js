6178e67585210bb1451e7649b9fb18de0fba956e
Moeen07 <moeenmoonis57@gmail.com>
2024-01-26 15:15:56 +0500

    Using Promises with .then and .catch

--- Week 1/Day 5/promises.js
+++ Week 1/Day 5/promises.js
@@ -3,21 +3,38 @@
 //   resolve("Successfully Resolved");
 // });
 
-// Suppose this is an API
-function getData(dataId, getNextData) {
+const getPromise = () => {
   return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log("data", dataId);
-      resolve("success..!");
-      if (getNextData) {
-        getNextData();
-      }
-    }, 3000);
+    console.log("This is a promise");
+    // resolve("Success...");
+    reject("Some error occured...");
   });
-}
+};
 
-result = getData(25);
-console.log(result);
+let promise = getPromise();
+promise.then((res) => {
+  console.log(res, "Promise Fullfilled");
+});
+
+promise.catch((err) => {
+  console.log("Promise was not fullfilled", err);
+});
+
+// Suppose this is an API
+// function getData(dataId, getNextData) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("data", dataId);
+//       resolve("success..!");
+//       if (getNextData) {
+//         getNextData();
+//       }
+//     }, 3000);
+//   });
+// }
+
+// result = getData(25);
+// console.log(result);
 
 //   getData(1, () => {
 //     getData(2, () => {
