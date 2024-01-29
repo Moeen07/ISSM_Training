f08a1a8b9c35bf7ce57f99a3d03ab417ff92fb31
Moeen07 <moeenmoonis57@gmail.com>
2024-01-29 15:28:41 +0500

    Added Middleware

--- Week 2/Day 1/Express Crash Course/index.js
+++ Week 2/Day 1/Express Crash Course/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const path = require("path");
 const members = require("./Members");
+const logger = require("./middleware/logger");
 const app = express();
+
+app.use(logger);
 
 app.get("/api/members", (req, res) => {
   res.json(members);
