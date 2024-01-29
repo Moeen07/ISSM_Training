7b278b320105e2b223198d6f8c5d64dc797a8731
Moeen07 <moeenmoonis57@gmail.com>
2024-01-29 13:39:36 +0500

    Reading file through a read stream

--- Week 2/Day 1/index.js
+++ Week 2/Day 1/index.js
@@ -4,9 +4,20 @@ const http = require("http");
 const server = http.createServer();
 
 server.on("request", (req, res) => {
-  let fs = require("fs");
-  fs.readFile("input.txt", (err, data) => {
-    if (err) return console.error(err);
-    console.log(data.toString());
+  //   fs.readFile("input.txt", (err, data) => {
+  //     if (err) return console.error(err);
+  //     res.end(data.toString());
+  //   });
+  const rStream = fs.createReadStream("input.txt");
+  rStream.on("data", (chunkData) => {
+    res.write(chunkData);
+  });
+  rStream.on("end", () => {
+    res.end();
+  });
+  rStream.on("error", () => {
+    res.end("file not found");
   });
 });
+
+server.listen(3000);
