9e945c37aa87b906a0229c5f88bcf849297634db
Moeen07 <moeenmoonis57@gmail.com>
2024-01-29 15:01:33 +0500

    Added Get all members api

---
+++ Week 2/Day 1/Express Crash Course/Members.js
@@ -0,0 +1,16 @@
+const members = [
+  {
+    id: 1,
+    name: "Moonis Moeen",
+    email: "example@gmail.com",
+    status: "isActive",
+  },
+  {
+    id: 2,
+    name: "Karen Smith",
+    email: "karen@gmail.com",
+    status: "isActive",
+  },
+];
+
+module.exports = members;
--- Week 2/Day 1/Express Crash Course/index.js
+++ Week 2/Day 1/Express Crash Course/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const path = require("path");
-
+const members = require("./Members");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+app.get("/api/members", (req, res) => {
+  res.json(members);
 });
+
+// Set a static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.PORT || 5000;
 
