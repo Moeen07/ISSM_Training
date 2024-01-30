533800f8bc75cbe243fbe70f0f4f29f9a8e58d87
Moeen07 <moeenmoonis57@gmail.com>
2024-01-30 15:03:10 +0500

    Added header & footer

--- Week 2/Day 2-3/NodeJs-Blog/server/routes/main.js
+++ Week 2/Day 2-3/NodeJs-Blog/server/routes/main.js
@@ -2,10 +2,17 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("index");
+  const locals = {
+    title: "NodeJS-Blog",
+    description: "Simple blog",
+  };
+  res.render("index", { locals });
 });
 router.get("/about", (req, res) => {
   res.render("about");
+});
+router.get("/contact", (req, res) => {
+  res.render("contact");
 });
 
 module.exports = router;
