109e54d72731970b443951df18df208bfc225225
Moeen07 <moeenmoonis57@gmail.com>
2024-01-31 00:28:49 +0500

    Added pagination

--- Week 2/Day 2-3/NodeJs-Blog/server/routes/main.js
+++ Week 2/Day 2-3/NodeJs-Blog/server/routes/main.js
@@ -3,15 +3,35 @@ const router = express.Router();
 const Post = require("../models/Post");
 
 //------------Home Route---------------------------
-router.get("/", (req, res) => {
-  const locals = {
-    title: "NodeJS-Blog",
-    description: "Simple blog",
-  };
-  res.render("index", { locals });
+router.get("/", async (req, res) => {
+  try {
+    const locals = {
+      title: "NodeJS-Blog",
+      description: "Simple blog",
+    };
+    let perPage = 2;
+    let page = req.query.page || 1;
 
-  //------------About Route---------------------------
+    const data = await Post.aggregate([{ $sort: { createdAt: -1 } }])
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec();
+
+    const count = await Post.countDocuments();
+    const nextPage = parseInt(page) + 1;
+    const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+    res.render("index", {
+      locals,
+      data,
+      current: page,
+      nextPage: hasNextPage ? nextPage : null,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
+//------------About Route---------------------------
 router.get("/about", (req, res) => {
   res.render("about");
 });
