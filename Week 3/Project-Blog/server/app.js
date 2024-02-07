0f9cc6320407e64fb2e2816ceb1aa5a1531d65bf
Moeen07 <moeenmoonis57@gmail.com>
2024-02-07 15:59:17 +0500

    Added DB and Sessions

--- Week 3/Project-Blog/server/app.js
+++ Week 3/Project-Blog/server/app.js
@@ -1,5 +1,32 @@
 require("dotenv").config();
 
 const express = require("express");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
 
 const app = express();
+
+const connectDB = require("../server/config/db");
+
+connectDB();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI,
+    }),
+  })
+);
+
+const PORT = 5000 || process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server running at ${PORT}`);
+});
