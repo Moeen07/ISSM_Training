7d48ada42239af9cbb5dfc5774de7ad2660af634
Moeen07 <moeenmoonis57@gmail.com>
2024-02-09 15:59:12 +0500

    Made Post route and Controller

---
+++ Week 3/Project-Blog/server/controllers/post-controller.js
@@ -0,0 +1,102 @@
+const Post = require("../models/Post");
+
+//-------Get All Posts----------------------------------------------------------
+const allPosts = async (req, res) => {
+  try {
+    let perPage = 2;
+    let page = req.query.page || 1;
+
+    const data = await Post.aggregate([{ $sort: { createdAt: -1 } }])
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec();
+
+    res.send(data);
+
+    const count = await Post.countDocuments();
+    const nextPage = parseInt(page) + 1;
+    const hasNextPage = nextPage <= Math.ceil(count / perPage);
+  } catch (error) {
+    console.log(error);
+  }
+};
+//---------------------------------------------------------------------------
+
+//-------Get Single Post----------------------------------------------------------
+
+const singlePost = async (req, res) => {
+  try {
+    let slug = req.params.id;
+    const data = await Post.findById({ _id: slug });
+    res.send(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+//---------------------------------------------------------------------------
+
+//-------------------------Search Post---------------------------------------
+
+const searchPost = async (req, res) => {
+  try {
+    let searchTerm = req.body.searchTerm;
+    const searchNoSpecialChar = searchTerm.replace(/[^a-zA-z0-9]/g, "");
+
+    const data = await Post.find({
+      $or: [
+        { title: { $regex: new RegExp(searchNoSpecialChar, "i") } },
+        { body: { $regex: new RegExp(searchNoSpecialChar, "i") } },
+      ],
+    });
+    res.send(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+//---------------------------------------------------------------------------
+
+//---------------------------Add Post----------------------------------------
+
+const addPost = async (req, res) => {
+  try {
+    await Post.create(newPost);
+  } catch (error) {
+    console.log(error);
+  }
+};
+//---------------------------Edit Post----------------------------------------
+
+const editPost = async (req, res) => {
+  try {
+    await Post.findByIdAndUpdate(req.params.id, {
+      title: req.body.title,
+      body: req.body.body,
+      updatedAt: Date.now(),
+    });
+    res.send("Post edited successfully");
+  } catch (error) {
+    console.log(error);
+  }
+};
+//-----------------------------------------------------------------------
+
+//---------------------------Delete Post----------------------------------------
+
+const deletePost = async (req, res) => {
+  try {
+    await Post.deleteOne({ _id: req.params.id });
+    res.send("Post Deleted");
+  } catch (error) {
+    console.log(error);
+  }
+};
+//-----------------------------------------------------------------------
+
+module.exports = {
+  allPosts,
+  singlePost,
+  searchPost,
+  addPost,
+  editPost,
+  deletePost,
+};
---
+++ Week 3/Project-Blog/server/routes/post.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+
+const authMiddleware = require("../middleware/auth-middleware");
+const permissionMiddleware = require("../middleware/permission-middleware");
+const {
+  allPosts,
+  singlePost,
+  searchPost,
+  addPost,
+  editPost,
+  deletePost,
+} = require("../controllers/post-controller");
+
+//------------Get All Posts-----------------------------------------
+router.get("/", allPosts);
+
+//-----------Get Single Post-------------------------------
+router.get("/post/:id", singlePost);
+
+//-------------------------------Search Post--------------------
+router.post("/search", searchPost);
+
+//------------Add a new Post-----------------------------------------
+router.post("/add-post", authMiddleware, addPost);
+
+//------------Edit Post-----------------------------------------
+router.put("/edit-post/:id", permissionMiddleware, authMiddleware, editPost);
+
+//------------Delete Post-----------------------------------------
+router.delete(
+  "/delete-post/:id",
+  permissionMiddleware,
+  authMiddleware,
+  deletePost
+);
